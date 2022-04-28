2e62b3e6669fc9764825579d685e9365f30eabca
zixiao18 <45200890+zixiao18@users.noreply.github.com>
2022-04-28 19:16:04 -0400

    test refined

--- arima.py
+++ arima.py
@@ -5,6 +5,46 @@ import pandas as pd
 import torch
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller as ADF
+
+
+def diff(timeseries):
+    timeseries_diff1 = np.diff(timeseries, axis=0)
+    timeseries_diff2 = np.diff(timeseries_diff1, axis=0)
+
+    timeseries_adf = ADF(timeseries.tolist())
+    timeseries_diff1_adf = ADF(timeseries_diff1.tolist())
+    timeseries_diff2_adf = ADF(timeseries_diff2.tolist())
+
+    print('timeseries_adf : ', timeseries_adf)
+    print('timeseries_diff1_adf : ', timeseries_diff1_adf)
+    print('timeseries_diff2_adf : ', timeseries_diff2_adf)
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(timeseries, label='Original', color='blue')
+    plt.plot(timeseries_diff1, label='Diff1', color='red')
+    plt.plot(timeseries_diff2, label='Diff2', color='purple')
+    plt.legend(loc='best')
+    plt.show()
+
+
+def autocorrelation(timeseries, lags):
+    fig = plt.figure(figsize=(12, 8))
+    ax1 = fig.add_subplot(211)
+    sm.graphics.tsa.plot_acf(timeseries, lags=lags, ax=ax1)
+    ax2 = fig.add_subplot(212)
+    sm.graphics.tsa.plot_pacf(timeseries, lags=lags, ax=ax2)
+    plt.show()
+
+
+def ARIMA_Model(timeseries, order):
+    model = ARIMA(timeseries, order=order)
+    return model.fit()
+
 
 window_size = 10
 enc_seq_len = window_size
@@ -27,7 +67,7 @@ e2s = np.zeros(lensize)
 for i in range(lensize):
     size = 50 + i * 10
     sizes[i] = size
-    df = pd.read_csv("TSLA.csv")
+    df = pd.read_csv("data/raw_data/TSLA.csv")
     X = df.iloc[:, 1:]
     X_simp = df['Close']
     Y = df["Close"]
@@ -37,56 +77,10 @@ for i in range(lensize):
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=1, shuffle=False)
 
-
-    import matplotlib.pyplot as plt
-    import statsmodels.api as sm
-    from statsmodels.tsa.arima.model import ARIMA
-    from statsmodels.tsa.seasonal import seasonal_decompose
-    from statsmodels.tsa.stattools import adfuller as ADF
-
-
-    def diff(timeseries):
-        timeseries_diff1 = np.diff(timeseries, axis=0)
-        timeseries_diff2 = np.diff(timeseries_diff1, axis=0)
-
-        timeseries_adf = ADF(timeseries.tolist())
-        timeseries_diff1_adf = ADF(timeseries_diff1.tolist())
-        timeseries_diff2_adf = ADF(timeseries_diff2.tolist())
-
-        print('timeseries_adf : ', timeseries_adf)
-        print('timeseries_diff1_adf : ', timeseries_diff1_adf)
-        print('timeseries_diff2_adf : ', timeseries_diff2_adf)
-
-        plt.figure(figsize=(12, 8))
-        plt.plot(timeseries, label='Original', color='blue')
-        plt.plot(timeseries_diff1, label='Diff1', color='red')
-        plt.plot(timeseries_diff2, label='Diff2', color='purple')
-        plt.legend(loc='best')
-        plt.show()
-
-
-    # diff(x_train)
-
-
-    def autocorrelation(timeseries, lags):
-        fig = plt.figure(figsize=(12, 8))
-        ax1 = fig.add_subplot(211)
-        sm.graphics.tsa.plot_acf(timeseries, lags=lags, ax=ax1)
-        ax2 = fig.add_subplot(212)
-        sm.graphics.tsa.plot_pacf(timeseries, lags=lags, ax=ax2)
-        plt.show()
-
-
     diff1 = np.diff(x_train, axis=0)
     diff1 = np.nan_to_num(diff1)
     diff2 = np.diff(diff1, axis=0)
     diff2 = np.nan_to_num(diff2)
-    # autocorrelation(diff1, 15)
-
-
-    def ARIMA_Model(timeseries, order):
-        model = ARIMA(timeseries, order=order)
-        return model.fit()
 
 
     decomposition = seasonal_decompose(x_train, period=7)
@@ -146,13 +140,13 @@ for i in range(lensize):
     val_error = np.abs(predict_seq / x_test - 1)
     e1 = np.mean(train_error)
     e2 = np.mean(val_error)
-    #print('The average training error is {:.2f}%. \nThe average testing error is {:.2f}%.'.format(e1 * 100, e2 * 100))
-    #print(train_error)
-    #print(val_error)
+    # print('The average training error is {:.2f}%. \nThe average testing error is {:.2f}%.'.format(e1 * 100, e2 * 100))
+    # print(train_error)
+    # print(val_error)
     e1s[i] = e1
     e2s[i] = e2
 
-plt.plot(sizes,e1s,label='training error')
-plt.plot(sizes,e2s,label='testing error')
+plt.plot(sizes, e1s, label='training error')
+plt.plot(sizes, e2s, label='testing error')
 plt.legend()
 plt.show()
