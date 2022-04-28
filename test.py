ae4576aba3f8b5e77ff7eae57cd1fd300011c2ee
zixiao18 <45200890+zixiao18@users.noreply.github.com>
2022-04-28 15:18:39 -0400

    hyperpara

--- test.py
+++ test.py
@@ -16,30 +16,8 @@ from torch.utils.data import DataLoader, TensorDataset
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_percentage_error
+from hyperparameters import *
 
-
-
-# hyperparameter
-window_size = 128
-enc_seq_len = window_size
-dec_seq_len = 2
-output_sequence_length = 1
-
-input_size = 5
-dim_val = 64 # embedding size
-dim_attn = 128
-lr = 0.001
-epochs = 20
-
-n_heads = 8
-
-n_decoder_layers = 2
-n_encoder_layers = 3
-batch_size = 64
-
-
-# time to vec
-time_embed_size = 2
 #=======================================================================
 stock_name = 'PFE'
 df = pd.read_csv(stock_name + "_cleaned.csv")
